64994424da13df4cee0a004fd1f168f019c3164b
rusrich <rubusy@yandex.ru>
2020-06-08 20:28:27 +0600

    Add reference for lesson 03 to Mmster

--- 03_lesson/01.test.js
+++
@@ -1,6 +0,0 @@
-describe('Проверка функции сложения положительных чисел в массиве', () => {
-  it('input data is Array', () => {
-    result = validateArray(number)
-    assert.equal(result, true)
-  });
-});
